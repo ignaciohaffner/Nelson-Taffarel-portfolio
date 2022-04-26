a235cd0ffc2e5ae2bc4a60acdd7e9de38a985ec5
ignaciohaffner <ignaciohaffner@gmail.com>
2022-04-26 17:17:33 -0300

    Photos

--- src/components/Fotos.jsx
+++ src/components/Fotos.jsx
@@ -7,31 +7,40 @@ const Fotos = () => {
         <div className="swiffy-slider slider-item-show3 slider-item-reveal slider-nav-touch slider-indicators-round slider-indicators-highlight">
     <ul className="slider-container">
         <li>
-            <div id="slide1"><img src="https://i.imgur.com/XtILJWD.jpg" alt="" /></div>
+            <div className='diapositiva' id="slide1"><img src="https://i.imgur.com/XtILJWD.jpg" alt="" /></div>
         </li>
         <li>
-            <div id="slide2"><img src="https://i.imgur.com/bUHuzkA.jpg" alt="" /></div>
+            <div className='diapositiva' id="slide2"><img src="https://i.imgur.com/bUHuzkA.jpg" alt="" /></div>
         </li>
         <li>
-            <div id="slide3"><img src="https://i.imgur.com/BmNKmCv.jpg" alt="" /></div>
+            <div className='diapositiva' id="slide3"><img src="https://i.imgur.com/BmNKmCv.jpg" alt="" /></div>
         </li>
         <li>
-            <div id="slide4"><img src="https://i.imgur.com/jV7CFz6.jpg" alt="" /></div>
+            <div className='diapositiva' id="slide4"><img src="https://i.imgur.com/jV7CFz6.jpg" alt="" /></div>
         </li>
         <li>
-            <div id="slide5"><img src="https://i.imgur.com/lE0hnbG.jpg" alt="" /></div>
+            <div className='diapositiva' id="slide5"><img src="https://i.imgur.com/lpWlmwp.jpg" alt="" /></div>
         </li>
         <li>
-            <div id="slide6"><img src="https://i.imgur.com/lpWlmwp.jpg" alt="" /></div>
+            <div className='diapositiva' id="slide6"><img src="https://i.imgur.com/lpWlmwp.jpg" alt="" /></div>
         </li>
         <li>
-            <div id="slide7"><img src="https://i.imgur.com/jhHE9cL.jpg" alt="" /></div>
+            <div className='diapositiva' id="slide7"><img src="https://i.imgur.com/jhHE9cL.jpg" alt="" /></div>
         </li>
         <li>
-            <div id="slide8"><img src="https://i.imgur.com/4vhjeIq.jpg" alt="" /></div>
+            <div className='diapositiva' id="slide8"><img src="https://i.imgur.com/4vhjeIq.jpg" alt="" /></div>
         </li>
         <li>
-            <div id="slide9"><img src="https://i.imgur.com/mo2VXE0.jpg" alt="" /></div>
+            <div className='diapositiva' id="slide9"><img src="https://i.imgur.com/mo2VXE0.jpg" alt="" /></div>
+        </li>
+        <li>
+            <div className='diapositiva' id="slide10"><img src="https://i.imgur.com/mo2VXE0.jpg" alt="" /></div>
+        </li>
+        <li>
+            <div className='diapositiva' id="slide11"><img src="https://i.imgur.com/TS2riKB.jpeg" alt="" /></div>
+        </li>
+        <li>
+            <div className='diapositiva' id="slide12"><img src="https://i.imgur.com/QU0Yba1.jpg" alt="" /></div>
         </li>
     </ul>
 
@@ -43,6 +52,9 @@ const Fotos = () => {
         <li className=""></li>
         <li className=""></li>
         <li className=""></li>
+        <li className=""></li>
+        <li className=""></li>
+        <li className=''></li>
         <li className=""></li>
         <li className=""></li>
         <li className=''></li>
